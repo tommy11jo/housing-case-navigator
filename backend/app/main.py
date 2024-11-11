2e1351f2fe99d118caa00683ceb947cc78d94f8d
Akshit Dewan <akshit@berkeley.edu>
2024-11-11 11:14:23 -0800

    fix CORS by setting allow_credentials to False

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ allowed_origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
